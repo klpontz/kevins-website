7040739b81129016a091ee9de817841ba96b8ce1
Kevin Rocci <kevinrocci@gmail.com>
2012-06-22 16:04:55 -0700

    Factor out append and prepend script, and clean up comments.

--- js/box-maker.js
+++ js/box-maker.js
@@ -1,7 +1,9 @@
-/*
- *  This is just a utitlity script so we can 
- *  add random content to masonry-ed layouts
- */
+ /* MAKE BOXES
+  * ======================================================= */
+ // This is a utitlity script to add
+ // random content to masonry-ed layouts.
+ // Compliments of David DeSandro
+ // http://vanilla-masonry.desandro.com/ 
 
 var boxMaker = {};
 
@@ -36,3 +38,35 @@ boxMaker.makeBoxes = function() {
   return boxes;
 };
 
+/* Prepend or Append Boxes
+ * ======================================================= */
+window.onload = function() {
+  var container = document.getElementById('content');
+  var wall = new Masonry( container, {
+    columnWidth: 94
+  });
+    
+  document.getElementById('prepend').onclick = function() {
+    // get an array of elements
+    var boxes = boxMaker.makeBoxes();
+
+    // prepend box to container
+    for (var i=0, len = boxes.length; i < len; i++) {
+      container.insertBefore( boxes[i], container.firstChild )
+    }
+      
+    wall.reload();
+  };
+
+
+  document.getElementById('append').onclick = function() {
+    // get an array of elements
+    var boxes = boxMaker.makeBoxes();
+
+    for (var i=0, len = boxes.length; i < len; i++) {
+      container.appendChild( boxes[i] );
+    }
+      
+    wall.appended( boxes );
+  };
+};
