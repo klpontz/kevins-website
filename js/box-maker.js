8d740fa6b3f996f998c24ee8dd73e3791803813a
Kevin Rocci <kevinrocci@gmail.com>
2012-06-22 22:36:51 -0700

    Function to generate more boxes and colors randomly.

--- js/box-maker.js
+++ js/box-maker.js
@@ -2,10 +2,23 @@
   * ======================================================= */
  // This is a utitlity script to add
  // random content to masonry-ed layouts.
- // Compliments of David DeSandro
+ // Built off code from David DeSandro
  // http://vanilla-masonry.desandro.com/ 
 
 var boxMaker = {};
+
+var colors = new Array();
+colors[0] = 'lightest';
+colors[1] = 'lighter';
+colors[2] = 'light';
+colors[3] = 'dark';
+colors[4] = 'darker';
+
+var boxType = new Array();
+boxType[0] = 'box';
+boxType[1] = 'horiRect';
+boxType[2] = 'vertRect';
+boxType[3] = 'bigBox';
 
 boxMaker.makeBoxes = function() {
   var boxes = [],
@@ -15,7 +28,13 @@ boxMaker.makeBoxes = function() {
     var box = document.createElement('div'),
         text = document.createTextNode( '1' );
     
-    box.className = 'brick horiRect blue' +  Math.ceil( Math.random()*3 );
+	var colorNumber = Math.floor(Math.random()*colors.length);
+	console.log (colorNumber);
+	
+	var boxNumber = Math.floor(Math.random()*boxType.length);
+	console.log (boxNumber);
+	
+    box.className = 'brick' + ' ' + boxType[boxNumber] + ' ' + colors[colorNumber];
     box.appendChild( text );
     // add box DOM node to document fragment
     boxes.push( box );
@@ -26,15 +45,14 @@ boxMaker.makeBoxes = function() {
 
 /* Prepend or Append Boxes
  * ======================================================= */
- 
  /*
  *  Add these buttons to the html for these functions.
 	  <p>
 	    <button id="prepend">Prepend new items</button>
 	    <button id="append">Append new items</button>
 	  </p>	
-*/
- 
+
+ */
 window.onload = function() {
   var container = document.getElementById('content');
   var wall = new Masonry( container, {
